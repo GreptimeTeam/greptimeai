50165c9be5c9d4675c54fc390883724444758037
yuanbohan <yuanbo.han@gmail.com>
2023-09-13 15:15:29 +0800

    feat: otel prometheus exporter

--- langchain/src/greptime_llm_langchain_instrument/__init__.py
+++ langchain/src/greptime_llm_langchain_instrument/__init__.py
@@ -1,5 +1,15 @@
 import time
-from typing import Union
+from queue import Queue
+from typing import Union, Dict
+
+import prometheus_client
+
+from opentelemetry.metrics import Observation, CallbackOptions
+
+# export PROMETHEUS_DISABLE_CREATED_SERIES=True
+prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
 
 
 class _TimeTable:
@@ -14,9 +24,9 @@ class _TimeTable:
     def remove(self, key: str) -> None:
         self.__tables.pop(key, None)
 
-    def latency(self, key: str) -> Union[float, None]:
+    def latency_in_millisecond(self, key: str) -> Union[float, None]:
         """
-        return latency in second if key exist, None if key not exist.
+        return latency in milli second if key exist, None if not exist.
         """
         now = time.time()
 
@@ -24,4 +34,21 @@ class _TimeTable:
         if start is None:
             return None
 
-        return now - start
+        return 1000 * (now - start)
+
+
+class _ObservationQueue:
+
+    def __init__(self):
+        self._queue = Queue()
+
+    def put(self, val: float, attrs: Dict) -> None:
+        self._queue.put(Observation(val, attributes=attrs))
+
+    def observation_callback(self):
+
+        def fn(_: CallbackOptions):
+            if not self._queue.empty():
+                yield self._queue.get()
+
+        return fn
--- langchain/src/greptime_llm_langchain_instrument/callback.py
+++ langchain/src/greptime_llm_langchain_instrument/callback.py
@@ -6,16 +6,59 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema.agent import AgentAction, AgentFinish
 from langchain.schema.messages import BaseMessage
 from langchain.schema.output import LLMResult
-from langchain.callbacks.openai_info import get_openai_token_cost_for_model
+from langchain.callbacks.openai_info import get_openai_token_cost_for_model, \
+    standardize_model_name
 
-from . import _TimeTable
+from prometheus_client import start_http_server
+
+from opentelemetry import metrics
+from opentelemetry.exporter.prometheus import PrometheusMetricReader
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+from . import _TimeTable, _ObservationQueue
 
 
 class GreptimeCallbackHandler(BaseCallbackHandler):
 
-    def __init__(self, verbose=False) -> None:
+    def __init__(self, port=8008, verbose=False) -> None:
         super().__init__()
-        self.__time_tables = _TimeTable()
+        self._time_tables = _TimeTable()
+        self._prompt_cost_queue = _ObservationQueue()
+        self._completion_cost_queue = _ObservationQueue()
+
+        start_http_server(port=port)
+
+        resource = Resource(
+            attributes={SERVICE_NAME: "greptime-langchain-observability"})
+        reader = PrometheusMetricReader()
+        provider = MeterProvider(resource=resource, metric_readers=[reader])
+        metrics.set_meter_provider(provider)
+        meter = metrics.get_meter("com.greptime.observability.langchain")
+        self._prompt_tokens_count = meter.create_counter(
+            "langchain_prompt_tokens",
+            description="counts the amount of prompt token",
+        )
+        self._completion_tokens_count = meter.create_counter(
+            "langchain_completion_tokens",
+            description="counts the amount of completion token",
+        )
+
+        self._requests_duration_histogram = meter.create_histogram(
+            name="langchain_llm_request_duration_milliseconds",
+            description="duration of requests of llm",
+        )
+
+        meter.create_observable_gauge(
+            callbacks=[self._prompt_cost_queue.observation_callback()],
+            name="langchain_prompt_tokens_cost",
+            description="prompt token cost in US Dollar",
+        )
+        meter.create_observable_gauge(
+            callbacks=[self._completion_cost_queue.observation_callback()],
+            name="langchain_completion_tokens_cost",
+            description="completion token cost in US Dollar",
+        )
 
     def __get_llm_repr(self, output: Any) -> str:
         """
@@ -70,7 +113,7 @@ class GreptimeCallbackHandler(BaseCallbackHandler):
         **kwargs: Any,
     ) -> Any:
         """Run when LLM starts running."""
-        self.__time_tables.set(run_id)
+        self._time_tables.set(run_id)
         ...
 
     def on_chat_model_start(
@@ -83,7 +126,7 @@ class GreptimeCallbackHandler(BaseCallbackHandler):
         **kwargs: Any,
     ) -> Any:
         """Run when Chat Model starts running."""
-        self.__time_tables.set(run_id)
+        self._time_tables.set(run_id)
         ...
 
     def on_llm_end(
@@ -99,11 +142,11 @@ class GreptimeCallbackHandler(BaseCallbackHandler):
 
         # Metric Table
 
-        langchain_prompt_tokens_count{llm,model}       # count
-        langchain_prompt_tokens_cost{llm,model}        # gauge
-        langchain_completion_tokens_count{llm,model}   # count
-        langchain_completion_tokens_cost{llm,model}    # gauge
-        langchain_callback_duration_seconds{llm,model} # histogram
+        langchain_prompt_tokens_count{llm,model}           # count
+        langchain_prompt_tokens_cost{llm,model}            # gauge
+        langchain_completion_tokens_count{llm,model}       # count
+        langchain_completion_tokens_cost{llm,model}        # gauge
+        langchain_llm_request_duration_seconds{llm,model}  # histogram
 
         # Trace
 
@@ -114,7 +157,7 @@ class GreptimeCallbackHandler(BaseCallbackHandler):
         message
 
         """
-        latency = self.__time_tables.latency(run_id)
+        latency = self._time_tables.latency_in_millisecond(run_id)
 
         output = (response.llm_output or {})
         token_usage = output.get("token_usage", {})
@@ -125,12 +168,26 @@ class GreptimeCallbackHandler(BaseCallbackHandler):
         model_name = output.get("model_name", None)
         llm = self.__get_llm_repr(output)
         if model_name is not None and llm == "openai":
+            model_name = standardize_model_name(model_name)
             try:
+                attrs = {
+                    "llm": llm,
+                    "model": model_name,
+                }
+                self._prompt_tokens_count.add(prompt_tokens, attrs)
+                self._completion_tokens_count.add(completion_tokens, attrs)
+
                 completion_cost = get_openai_token_cost_for_model(
                     model_name, completion_tokens, is_completion=True)
                 prompt_cost = get_openai_token_cost_for_model(
                     model_name, prompt_tokens)
-            except Exception:
+
+                self._completion_cost_queue.put(completion_cost, attrs)
+                self._prompt_cost_queue.put(prompt_cost, attrs)
+
+                self._requests_duration_histogram.record(latency, attrs)
+            except Exception as ex:
+                print(f"on_llm_end exception: {ex}")
                 pass
 
     def on_llm_error(
