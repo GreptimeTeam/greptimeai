af7035b5101c2dbfc477040046d575c0a15bfb43
Anyc66666666 <92040980+Anyc66666666@users.noreply.github.com>
2023-11-23 15:30:42 +0800

    refactor: refactor openai tracker (#71)
    * refactor: refactor openai tracker
    
    * fix: correct obj type
    
    * chore: solve code lint
    
    * chore: remove unused code

---
+++ src/greptimeai/tracker/openai_tracker/embedding.py
@@ -0,0 +1,57 @@
+from typing import Optional, Callable, Tuple, Any, Dict
+
+import openai
+from openai import OpenAI
+from openai.types import CreateEmbeddingResponse
+
+from greptimeai import (
+    _COMPLETION_COST_LABEL,
+    _COMPLETION_TOKENS_LABEL,
+    _PROMPT_COST_LABEl,
+    _PROMPT_TOKENS_LABEl,
+)
+from greptimeai.tracker.openai_tracker.public import (
+    pre_extractor,
+    post_extractor,
+)
+from greptimeai.utils.openai.token import (
+    get_openai_token_cost_for_model,
+)
+
+
+class EmbeddingExtractor:
+    def __init__(
+        self,
+        verbose: bool,
+        client: Optional[OpenAI] = None,
+    ):
+        self.verbose = verbose
+        self.span_name = "embeddings.create"
+        self.obj = client.embeddings if client else openai.embeddings
+        self.method_name = "create"
+        self.pre_extractor = pre_extractor(self.req_call_func, self.verbose)
+        self.post_extractor = post_extractor(self.res_call_func)
+
+    def req_call_func(self) -> Tuple[str, Callable]:
+        param_name = ""
+
+        def execute_function(param: Any) -> Any:
+            return
+
+        return param_name, execute_function
+
+    def res_call_func(self, resp) -> Dict[str, Any]:
+        if isinstance(resp, CreateEmbeddingResponse):
+            usage = {}
+            if resp.usage:
+                usage[_PROMPT_TOKENS_LABEl] = resp.usage.prompt_tokens
+            usage[_PROMPT_COST_LABEl] = get_openai_token_cost_for_model(
+                resp.model, resp.usage.prompt_tokens, False
+            )
+            usage[_COMPLETION_TOKENS_LABEL] = 0
+            usage[_COMPLETION_COST_LABEL] = 0
+
+            attrs = {
+                "usage": usage,
+            }
+            return attrs
