84cb5e2270dbaf7c24c935e7dfde183b6eec3b37
yuanbohan <yuanbo.han@gmail.com>
2023-11-24 10:26:38 +0800

    feat: add openai audio extractor

--- src/greptimeai/extractor/openai_extractor/__init__.py
+++ src/greptimeai/extractor/openai_extractor/__init__.py
@@ -13,6 +13,7 @@ from greptimeai.extractor import BaseExtractor, Extraction
 from greptimeai.utils.openai.token import get_openai_token_cost_for_model
 
 _X_USER_ID = "x-user-id"
+# TODO(yuanbohan): support more x headers in extra_headers. e.g. x-trace-id, x-span-id, x-span-context
 
 
 class OpenaiExtractor(BaseExtractor):
@@ -20,6 +21,14 @@ class OpenaiExtractor(BaseExtractor):
         self.obj = obj
         self.span_name = span_name
         self.method_name = method_name
+
+    @staticmethod
+    def get_user_id(**kwargs) -> Optional[str]:
+        user_id = kwargs.get("user", None)
+        extra_headers = kwargs.get("extra_headers", None)
+        if extra_headers and _X_USER_ID in extra_headers:
+            user_id = extra_headers[_X_USER_ID]
+        return user_id
 
     @staticmethod
     def extract_usage(
@@ -31,16 +40,18 @@ class OpenaiExtractor(BaseExtractor):
             return res
 
         prompt_tokens = usage.get("prompt_tokens", 0)
-        res[_PROMPT_TOKENS_LABEl] = prompt_tokens
-        res[_PROMPT_COST_LABEl] = get_openai_token_cost_for_model(
-            model, prompt_tokens, False
-        )
+        if prompt_tokens > 0:
+            res[_PROMPT_TOKENS_LABEl] = prompt_tokens
+            res[_PROMPT_COST_LABEl] = get_openai_token_cost_for_model(
+                model, prompt_tokens, False
+            )
 
         completion_tokens = usage.get("completion_tokens", 0)
-        res[_COMPLETION_TOKENS_LABEL] = completion_tokens
-        res[_COMPLETION_COST_LABEL] = get_openai_token_cost_for_model(
-            model, completion_tokens, True
-        )
+        if completion_tokens > 0:
+            res[_COMPLETION_TOKENS_LABEL] = completion_tokens
+            res[_COMPLETION_COST_LABEL] = get_openai_token_cost_for_model(
+                model, completion_tokens, True
+            )
         return res
 
     def pre_extract(self, *args, **kwargs) -> Extraction:
@@ -53,15 +64,13 @@ class OpenaiExtractor(BaseExtractor):
                 extra_headers in kwargs: _X_USER_ID is a custom header that is used to identify the user,
                 which has higher priority than the 'user' in the kwargs
         """
-        user_id = kwargs.get("user", None)
-        extra_headers = kwargs.get("extra_headers", None)
-        if extra_headers and _X_USER_ID in extra_headers:
-            user_id = extra_headers[_X_USER_ID]
+        span_attrs = {}
+        if "model" in kwargs:
+            span_attrs[_MODEL_LABEL] = kwargs["model"]
 
-        span_attrs = {
-            _MODEL_LABEL: kwargs.get("model", None),
-            _USER_ID_LABEL: user_id,
-        }
+        user_id = OpenaiExtractor.get_user_id(**kwargs)
+        if user_id:
+            span_attrs[_USER_ID_LABEL] = user_id
 
         event_attrs = {**kwargs}
         if len(args) > 0:
@@ -82,7 +91,10 @@ class OpenaiExtractor(BaseExtractor):
 
         Args:
 
-            resp: response from openai api, which MUST inherit the BaseModel class
+            resp: response from openai api, which MUST inherit the BaseModel class so far.add()
+
+
+        TODO: support response which DOSE NOT inherit the BaseModel class
         """
         try:
             dump = resp.model_dump()
@@ -90,17 +102,18 @@ class OpenaiExtractor(BaseExtractor):
             logger.error(f"Failed to call model_dump for {resp}: {e}")
             dump = {}
 
+        span_attrs = {}
+
+        model = dump.get("model", None)
+        if model:
+            span_attrs[_MODEL_LABEL] = model
+
         usage = dump.get("usage", {})
-        model = dump.get("model")
         if usage and model:
             usage = OpenaiExtractor.extract_usage(model, usage)
+            span_attrs.update(usage)
+            dump["usage"] = usage
 
-        span_attrs = {
-            _MODEL_LABEL: model,
-            **usage,
-        }
-
-        dump["usage"] = usage
         return Extraction(span_attributes=span_attrs, event_attributes=dump)
 
     def get_func_name(self) -> str:
