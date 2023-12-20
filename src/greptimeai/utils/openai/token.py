c3db4a730191473c1a72f8bc47695a68737f2f7b
Anyc66666666 <1662048992@qq.com>
2023-12-20 15:20:24 +0800

    chore: optimize token func

--- src/greptimeai/utils/openai/token.py
+++ src/greptimeai/utils/openai/token.py
@@ -196,19 +196,17 @@ def num_tokens_from_messages(
         )
         tokens_per_name = -1  # if there's a name, the role is omitted
     elif "gpt-3.5-turbo" in model:
-        print(
-            "Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613."
+        logger.warning(
+            "gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613."
         )
         return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
     elif "gpt-4" in model:
-        print(
-            "Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613."
+        logger.warning(
+            "gpt-4 may update over time. Returning num tokens assuming gpt-4-0613."
         )
         return num_tokens_from_messages(messages, model="gpt-4-0613")
     else:
-        raise NotImplementedError(
-            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
-        )
+        return 0
     num_tokens = 0
     for message in messages:
         num_tokens += tokens_per_message
