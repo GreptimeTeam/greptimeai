49eb2fd5716fe39962d538dbbf7fda3018ed91ff
Anyc66666666 <1662048992@qq.com>
2023-12-25 19:57:50 +0800

    feat: add test for tool call

--- tests/integrations/openai_tracker/test_chat_parameters.py
+++ tests/integrations/openai_tracker/test_chat_parameters.py
@@ -13,6 +13,10 @@ def _truncate_tables():
     yield
 
 
+def get_lowercase_letters(letters: str) -> str:
+    return letters.lower()
+
+
 def test_chat_completion_n_gt_1(_truncate_tables):
     user_id = str(uuid.uuid4())
     model = "gpt-3.5-turbo"
@@ -153,3 +157,73 @@ def test_chat_completion_stop(_truncate_tables):
             message = choice.get("message")
             assert message
             assert stop not in message.get("content")
+
+
+def test_chat_completion_tool_call(_truncate_tables):
+    user_id = str(uuid.uuid4())
+    model = "gpt-3.5-turbo"
+    resp = sync_client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "GREPTIMEAI",
+            },
+            {"role": "function", "name": "get_lowercase_letters", "content": "letters"},
+        ],
+        model=model,
+        user=user_id,
+        seed=1,
+        tools=[{
+            "type": "function",
+            "function": {
+                "description": "get lowercase letters",
+                "name": "get_lowercase_letters",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "letters": {
+                            "type": "string",
+                            "description": "uppercase letters"
+                        }
+                    },
+                    "required": ["letters"]
+                }
+            }
+        }],
+        tool_choice="auto",
+    )
+
+    assert resp.choices[0].message.content == "greptimeai"
+
+    collector.otel._force_flush()
+
+    trace = get_trace_data_with_retry(user_id=user_id, retry=3)
+
+    assert trace is not None
+
+    assert "greptimeai" == trace.get("resource_attributes", {}).get("service.name")
+    assert "openai_completion" == trace.get("span_name")
+    assert "openai" == trace.get("span_attributes", {}).get("source")
+
+    assert ["client.chat.completions.create", "end"] == [
+        event.get("name") for event in trace.get("span_events", [])
+    ]
+
+    assert resp.model == trace.get("model")
+    assert resp.model.startswith(model)
+
+    assert resp.usage
+    assert resp.usage.prompt_tokens == trace.get("prompt_tokens")
+    assert resp.usage.completion_tokens == trace.get("completion_tokens")
+
+    for event in trace.get("span_events", []):
+        if event.get("name") == "end":
+            attrs = event.get("attributes")
+            assert attrs
+            assert attrs["choices"]
+            choice = attrs["choices"][0]
+            assert choice
+            assert choice.get("finish_reason") == "tool_calls"
+            message = choice.get("message")
+            assert message
+            assert message.get("content") == "greptimeai"
