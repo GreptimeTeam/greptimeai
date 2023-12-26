48585647524c2e8e0ffb386b2bb67247f6947318
Anyc66666666 <1662048992@qq.com>
2023-12-26 17:33:19 +0800

    chore: optimize audio,encoder and sql

--- src/greptimeai/utils/json_encoder.py
+++ src/greptimeai/utils/json_encoder.py
@@ -1,9 +1,14 @@
 import json
 from io import BufferedReader
 
+from greptimeai import logger
+
 
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, BufferedReader):
-            return obj.name
+            if hasattr(obj, "name"):
+                return obj.name
+            logger.warning("buffer obj has no name attribute")
+            return {}
         return super().default(obj)
